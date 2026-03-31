1d1d1e6d7bd30d122b909951a97499a7b66e6ac5
marvahan <mahan4300@uib.no>
2026-03-31 23:39:22 +0200

    Lagde koden for fetchRandomCocktail()

--- script.js
+++ script.js
@@ -115,7 +115,11 @@ Fetch a Random Cocktail (backup in case nothing is found by the search)
 Returns a Promise that resolves to cocktail object
 */
 function fetchRandomCocktail() {
-    // Fill in
+    return fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        .then(response => response.json())
+        .then(data => {
+            return data.drinks[0];
+        });
 }
 
 /*
